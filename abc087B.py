805f8ccdadbae3ea65226f2086fa1497f4a1a1f0
Visal Kulathunga <vkkulathunga@gmail.com>
2021-02-17 10:15:57 +0900

    abc087b Advanced

--- abc087B.py
+++ abc087B.py
@@ -8,8 +8,8 @@ x = int(input())
 num_combination = 0
 
 for n_500 in range(0,a+1):          #num of 500yen coins
-    for n_100 in range(0, b+1):     #num of 100yen coins
-        for n_50 in range(0,c+1):   #num of 50yen coins
-            if n_500*500+n_100*100+n_50*50 == x:
-                num_combination += 1
+    for n_100 in range(0, b+1):
+        n_50 = (x - n_500*500 - n_100*100)//50
+        if n_50<=c and n_50>=0:
+            num_combination += 1
 print(num_combination)
